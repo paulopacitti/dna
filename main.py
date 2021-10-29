69e4650b50e4b6e0732ce14151eb86ff1953a21a
paulopacitti <ppacitti@oulook.com>
2021-10-29 10:02:55 -0300

    feat: adds input handling and dna indexation

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from sys import stdin
 
 def main():
-    chains = []
+    chains_dict = {}
+    chains_list = []
     for input in stdin:
         if input == '': # If empty string is read then stop the loop
             break
         else:
-            chains.append(input)
+            if "start_" + input[0:4] not in chains_dict:
+                chains_dict["start_" + input[0:4]] = input[:-1]
+            else:
+                chains_dict["start_" + input[0:4]].append(input[:-1])
+            if "end_" + input[-5:-1] not in chains_dict:
+                chains_dict["end_" + input[-5:-1]] = input[:-1]
+            else:
+                chains_dict["end_" + input[-5:-1]].append(input[:-1])
+            chains_list.append(input[:-1])
+    # match()
 
+# def match(dna_list, dict):
+#     for e in dna_list:
 
-def match():
+if __name__ == "__main__":
+    main()
