e568787d1649bb7593def60ae8f454088d5cdeb1
Caio Henrique Pardal <pardalcaio@hotmail.com>
2021-11-02 21:18:43 -0300

    Rename variables and remove unused codes

--- main.py
+++ main.py
@@ -16,41 +16,39 @@ def main():
             else:
                 chains_dict["end_" + input[-5:-1]].append(input[:-1])
             chains_list.append(input[:-1])
-    result = match(chains_list, chains_dict)
-    print(result)
+    dna_list = match(chains_list, chains_dict)
+    
+    for dna in dna_list:
+        print(dna)
 
-def match_recursive(curr_chain, final_combinations, chains_dict):
+def match_recursive(curr_chain, combinations_list, chains_dict):
     possible_combinations = combinations(curr_chain, chains_dict)
     if not possible_combinations:
         return curr_chain
 
-    for e in possible_combinations:
-        final_combinations.append(match_recursive(curr_chain + e, final_combinations, chains_dict))
+    for dna in possible_combinations:
+        combinations_list.append(match_recursive(curr_chain + dna, combinations_list, chains_dict))
     
     return curr_chain
 
 def match(dna_list, chains_dict):
     final_combinations = []
-    for e in dna_list:
-        if not combinations(e, chains_dict):
-            final_combinations.append(e)
+    for dna in dna_list:
+        if not combinations(dna, chains_dict):
+            final_combinations.append(dna)
         else:
-            match_recursive(e, final_combinations, chains_dict)
+            match_recursive(dna, final_combinations, chains_dict)
     
-    print(final_combinations)
-
     remove_list = []
-    for i in final_combinations:
-        for x in final_combinations:
-            if x.find(i) != -1:
-                if len(x) > len(i):
-                    remove_list.append(i)
-                elif len(x) < len(i): 
+    for x in final_combinations:
+        for y in final_combinations:
+            if y.find(x) != -1:
+                if len(y) > len(x):
                     remove_list.append(x)
+                elif len(y) < len(x): 
+                    remove_list.append(y)
     
-    print(remove_list)
-    
-    return [item for item in final_combinations if item not in remove_list]
+    return [combination for combination in final_combinations if combination not in remove_list]
 
 def combinations(chain, chains_dict):
     possible_key = "start_" + chain[-4:]
